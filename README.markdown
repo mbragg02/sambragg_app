833877d4473be2ca004028fa974efb2163b7244d
Michael Bragg <mike.a.bragg@gmail.com>
2011-04-14 23:53:47 +0100

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# sambragg_app
+
+This is the V2 version of the site for singer/songwriter [Sam Bragg](http://www.sambragg.co.uk)
+by Michael Bragg.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SambraggApp::Application.config.secret_token = '24dd823273cd402070a0d1dd5aa35b96878d11eb1a669fc42dd6ee63d5d50d8fb06fc44824132b318c8ffd362649b24eae598bf32a3938d6472f2689d9dc1044'
